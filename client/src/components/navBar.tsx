bfd2b0c306bbcf1183b2b6b8854fb5b60fbc76f2
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 21:18:19 +0000

    Implemented: Structure Code for Implementing Top and Side Nav Bar

---
+++ client/src/components/navBar.tsx
@@ -0,0 +1,18 @@
+function NavBar() {
+
+    return (
+
+        <div className={"NavBar"}>
+            <div className={"TopNavBar"}>
+
+            </div>
+
+            <div className={"SideNavBar"}>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default NavBar
