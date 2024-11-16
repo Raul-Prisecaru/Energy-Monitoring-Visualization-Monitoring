295b2a88a5d0dc75e29081305a6a3d7bd82bedff
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 19:32:40 +0000

    Removed Template code

---
+++ client/src/pages/homePage.tsx
@@ -0,0 +1,15 @@
+import "./styles/homePageStyle.css"
+function HomePage() {
+
+
+    return (
+        <div>
+            <div className={"Title"}>
+                <h1>Welcome to your one-stop shop to managing your energy consumption usage</h1>
+            </div>
+        </div>
+    )
+
+}
+
+export default HomePage
