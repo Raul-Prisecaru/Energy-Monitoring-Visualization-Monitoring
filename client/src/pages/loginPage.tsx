ae815cd0934dfed14ca73292d2b304d7a0c67029
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-21 19:01:57 +0000

    Changed: Removed unnecessary fields for login

--- client/src/pages/loginPage.tsx
+++ client/src/pages/loginPage.tsx
@@ -6,16 +6,6 @@ function LoginPage() {
         <div className={"Input-Form"}>
             <form>
                 <div>
-                    <label>First Name</label>
-                    <input type={"text"}/> <br/>
-                </div>
-
-                <div>
-                    <label>Last Name</label>
-                    <input type={"text"}/> <br/>
-                </div>
-
-                <div>
                     <label>Email</label>
                     <input type={"text"}/> <br/>
                 </div>
