bdea37c9a315ce2a2a58b789740e5f39214d2e7a
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-21 19:02:32 +0000

    Implemented: Form and Input for signup Information

---
+++ client/src/pages/signupPage.tsx
@@ -0,0 +1,33 @@
+import "./styles/loginPageStyle.css"
+
+function SignupPage() {
+
+    return (
+        <div className={"Input-Form"}>
+            <form>
+                <div>
+                    <label>First Name</label>
+                    <input type={"text"}/> <br/>
+                </div>
+
+                <div>
+                    <label>Last Name</label>
+                    <input type={"text"}/> <br/>
+                </div>
+
+                <div>
+                    <label>Email</label>
+                    <input type={"text"}/> <br/>
+                </div>
+
+                <div>
+                    <label>password</label>
+                    <input type={"text"}/>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default SignupPage
