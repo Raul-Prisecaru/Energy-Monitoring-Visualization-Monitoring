5747a3c531ab3f2258af0b8d1bd8979665fc66c7
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-24 01:20:11 +0000

    Implemented: Added First Name and Last Name to the userSchema

--- server/model/userAccount.ts
+++ server/model/userAccount.ts
@@ -3,6 +3,18 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
 
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
     username: {
         type: String,
         required: true,
