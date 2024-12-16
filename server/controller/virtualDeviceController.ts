6657cf48bdf0d967255d9516a43338aaeb4646fe
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 18:07:53 +0000

    Implemented: Functions for creating, updating, and deleting virtual Device

--- server/controller/virtualDeviceController.ts
+++ server/controller/virtualDeviceController.ts
@@ -6,8 +6,12 @@ exports.createDevice = async (req, res) => {
     const newDevice = new virtualDevice(deviceData)
 
     try {
+        await newDevice.save()
+
+        res.status(201).json({message: "Successfully created virtual Device"})
 
     } catch (err) {
+        res.status(500).json({message: "Failed to create virtual device: " +  err})
 
     }
 
@@ -16,16 +20,18 @@ exports.createDevice = async (req, res) => {
 // Function to Update Device
 exports.updateDevice = async (req, res) => {
     try {
-
+        await virtualDevice.findByIdAndUpdate(req.param.id, req.body)
+        res.status(201).json({message: "Successfully found and updated virtual device"})
     } catch (err) {
-
+        res.status(500).json({message: "Failed to find or update virtual device: " +  err})
     }
 };
 
 // Function to Delete Device
 exports.deleteDevice = async (req, res) => {
     try {
-
+        await virtualDevice.findByIdAndDelete(req.param.id)
+        res.status(201).json({message: "Successfully found and deleted virtual Device"})
     } catch (err) {
 
     }
