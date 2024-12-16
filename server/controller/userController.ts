48fa865cd75a992dfb9c339d5517341c7cfef0af
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 17:09:48 +0000

    Implemented: Function to delete user

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -17,13 +17,20 @@ exports.createUser = async (req, res) => {
 // Function to Update User
 exports.updateUser = async (req, res) => {
     try {
-        const findUser = User.findByIdAndUpdate(req.param.id, req.body)
+        await User.findByIdAndUpdate(req.param.id, req.body)
         res.status(200).json({message: "Successfully found and updated the user's information"})
     } catch (err) {
         res.status(500).json({message: "Failed to find or update user: " + err})
+    }
 };
 
 // Function to Delete User
 exports.deleteUser = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.param.id)
+        res.status(200).json({message: "Successfully found and deleted the user"})
+    } catch (err) {
+        res.status(500).json({message: "Failed to find or delete user: " + err})
+    }
 
 };
