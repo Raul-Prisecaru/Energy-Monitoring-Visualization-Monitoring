7ffc41ca0d691393554fef20f63b1b458978e7ec
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 16:45:10 +0000

    Implemented: function to create user

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -2,6 +2,15 @@ const User = require("../model/userAccount");
 
 // Function to Create User
 exports.createUser = async (req, res) => {
+    const userData = req.body;
+    const newUser = new User(userData)
+
+    try {
+        await newUser.save()
+        res.status(201).json({message: "Successfully Created User"})
+    } catch (err) {
+        res.status(500).json({message: "Failed to create user: " + err})
+    }
 
 };
 
