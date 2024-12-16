255fd2aabf0f1d8c73e0636111e0e4f61d792f04
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 16:35:00 +0000

    Added: Empty Functions for creating, updating, and deleting users

---
+++ server/controller/userController.ts
@@ -0,0 +1,16 @@
+const User = require("../model/userAccount");
+
+// Function to Create User
+exports.createUser = async (req, res) => {
+
+};
+
+// Function to Update User
+exports.updateUser = async (req, res) => {
+
+};
+
+// Function to Delete User
+exports.deleteUser = async (req, res) => {
+
+};
