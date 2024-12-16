942297dc0699ef9a2442b8bb1bc7cb0da76dd555
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 16:59:36 +0000

    Implemented: Function to update user information

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -16,7 +16,11 @@ exports.createUser = async (req, res) => {
 
 // Function to Update User
 exports.updateUser = async (req, res) => {
-
+    try {
+        const findUser = User.findByIdAndUpdate(req.param.id, req.body)
+        res.status(200).json({message: "Successfully found and updated the user's information"})
+    } catch (err) {
+        res.status(500).json({message: "Failed to find or update user: " + err})
 };
 
 // Function to Delete User
