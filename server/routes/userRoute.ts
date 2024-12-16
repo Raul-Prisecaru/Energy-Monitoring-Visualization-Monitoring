3ba6376126fb3b06f2c8e121cfed440ff8d8f7db
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 18:33:30 +0000

    Added: router post, put, delete

---
+++ server/routes/userRoute.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const userController = require("../controller/userController");
+
+const router = express.Router();
+
+router.post()
+
+router.put()
+
+router.delete()
