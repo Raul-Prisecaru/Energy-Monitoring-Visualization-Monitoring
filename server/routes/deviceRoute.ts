e0fc8f4e11550dea8f48b0c2250e860f33b75c84
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 18:41:59 +0000

    Implemented: Routes for creating, updating, and deleting

---
+++ server/routes/deviceRoute.ts
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const deviceController = require("../controller/virtualDeviceController");
+
+const router = express.Router();
+
+// Router to create device
+router.post("/", deviceController.createDevice)
+
+// Router to update device, Takes ID Param
+router.put("/:id", deviceController.updateDevice)
+
+// Router to delete device, Takes ID Param
+router.delete("/:id", deviceController.deleteDevice)
+
+// Export Router
+module.exports = router;
