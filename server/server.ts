c2eeb5904c4b5e6d4559c9dbf0f6e6777dcf53f1
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-16 21:50:04 +0000

    bugFix: Fixed issue where it couldn't make a request

--- server/server.ts
+++ server/server.ts
@@ -2,16 +2,16 @@ const express = require("express");
 require("dotenv").config();
 const userRouters = require("./routes/userRoute.ts")
 const mongoose = require("mongoose")
-const server = express();
+const app = express();
 const PORT = process.env.PORT
 
 mongoose.connect(process.env.DATABASE_URL)
-server.use(express.json)
+app.use(express.json())
 
 
-server.use("/api/user", userRouters)
+app.use("/api/user", userRouters)
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listening to Port ${PORT}`)
 
 })
