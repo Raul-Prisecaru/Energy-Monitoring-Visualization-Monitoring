baf03686891c869e7279b6373dcf8f721d70d63e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-21 23:38:50 +0000

    Added: heading and imported css file

--- client/src/pages/devicePage.tsx
+++ client/src/pages/devicePage.tsx
@@ -1,5 +1,14 @@
+import "./styles/devicePageStyle.css"
+
 function DevicePage() {
 
+    return (
+        <div>
+            <div className={"Heading"}>
+                <h1>Virtual Devices</h1>
+            </div>
+        </div>
+    )
 }
 
 
