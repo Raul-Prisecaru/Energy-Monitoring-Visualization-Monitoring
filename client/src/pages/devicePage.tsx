857a50a2f45806864edb745d7b744d8801e4f2a2
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-22 00:31:05 +0000

    Implemented: Button with fetch post request to add device

--- client/src/pages/devicePage.tsx
+++ client/src/pages/devicePage.tsx
@@ -2,6 +2,32 @@ import "./styles/devicePageStyle.css"
 
 function DevicePage() {
 
+    const addDeviceButton = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await fetch("http://localhost:3001/api/device", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    deviceType,
+                    energyUsage,
+                    energyDate,
+                })
+            });
+
+            if (response.ok) {
+                alert("Data submitted successfully")
+            } else {
+                alert("Failed to submit data")
+            }
+        } catch (error) {
+            alert("An Error occurred when submitting data:" + error)
+        }
+    }
+
     return (
         <div>
             <div>
