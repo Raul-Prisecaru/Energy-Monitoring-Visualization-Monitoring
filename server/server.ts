77189e2f1b0bb5b6c66789c52ef4170f142e4dbe
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-22 00:28:42 +0000

    Added: device routers

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const userRouters = require("./routes/userRoute.ts")
+const deviceRouters = require("./routes/deviceRoute.ts")
 const mongoose = require("mongoose")
 const app = express();
 const PORT = process.env.PORT
@@ -10,6 +11,7 @@ app.use(express.json())
 
 
 app.use("/api/user", userRouters)
+app.use("/api/device", deviceRouters)
 
 app.listen(PORT, () => {
     console.log(`Listening to Port ${PORT}`)
