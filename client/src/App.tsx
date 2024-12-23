95d2ab714fb882f2949f242d0dbf041f5921fce6
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-23 22:08:49 +0000

    Added: Route for ProfileManagement

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/loginPage.tsx";
 import SignupPage from "./pages/signupPage.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DevicePage from "./pages/devicePage.tsx";
+import ProfileManagementPage from "./pages/profileManagementPage.tsx";
 function App() {
 
   return (
@@ -13,9 +14,9 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<HomePage />}></Route>
                 <Route path={"login"} element={<LoginPage />}></Route>
-                <Route path={"login"} element={<LoginPage />}></Route>
                 <Route path={"sign-up"} element={<SignupPage />}></Route>
                 <Route path={"devices"} element={<DevicePage />}></Route>
+                <Route path={"ProfileManagement"} element={<ProfileManagementPage />}> </Route>
             </Routes>
 
 
