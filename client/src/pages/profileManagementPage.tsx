9d8063d74ca39724a2e4f8ee6ed8475b4b8f236b
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-23 21:58:22 +0000

    Added: Basic Functions to set the value of the useState

--- client/src/pages/profileManagementPage.tsx
+++ client/src/pages/profileManagementPage.tsx
@@ -7,6 +7,26 @@ function ProfileManagementPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
 
+    const handleFirstNameChange = async (e) => {
+        setFirstName(e.target.value);
+    }
+
+    const handleLastNameChange = async (e) => {
+        setLastName(e.target.value);
+    }
+
+    const handleUsernameChange = async (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handleEmailChange = async (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = async (e) => {
+        setPassword(e.target.value)
+    }
+
     return (
         <div>
 
