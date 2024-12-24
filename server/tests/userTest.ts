870286a631ee370fcbbc7e5f22aa12c384f941bc
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-24 03:01:25 +0000

    Implemented: Simple response check for 201

--- server/tests/userTest.ts
+++ server/tests/userTest.ts
@@ -1,7 +1,27 @@
 import {describe} from "node:test";
-
+import {stringify} from "node:querystring";
+const assert = require('chai').assert
 describe("Testing the Routes of User", () => {
     describe("Testing User can be created", () => {
+        it('should create the user successfully', async () => {
+            const response = await fetch("http://localhost:3001/api/user", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+
+                body: JSON.stringify({
+                    firstName: "userFirst",
+                    lastName: "userLast",
+                    username: "userFirstLast",
+                    email: "userFirstLast@testemail.com",
+                    password: "userFirstLast123!"
+                })
+            })
+
+            assert.strictEqual(response.status, 201, "Expected 201 success response")
+
+        });
 
     })
 
