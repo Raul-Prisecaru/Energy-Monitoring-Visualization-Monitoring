53df21a53a50db7db00c5e05a8baaf023f215396
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-24 02:43:16 +0000

    Added: Testing Structure

---
+++ server/tests/userTest.ts
@@ -0,0 +1,23 @@
+import {describe} from "node:test";
+
+describe("Testing the Routes of User", () => {
+    describe("Testing User can be created", () => {
+
+    })
+
+    describe("We can retrieve one user based on ID", () => {
+
+    })
+
+    describe("We can retrieve all created users", () => {
+
+    })
+
+    describe("We can update user based on ID", () => {
+
+    })
+
+    describe("We can delete user based on ID", () => {
+
+    })
+})
