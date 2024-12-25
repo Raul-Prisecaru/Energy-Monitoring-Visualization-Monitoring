de4fb57936b23f14a7800d058873963063c9ac0b
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-25 23:46:19 +0000

    Added: Dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import SignupPage from "./pages/signupPage.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DevicePage from "./pages/devicePage.tsx";
 import ProfileManagementPage from "./pages/profileManagementPage.tsx";
+import UsageAreaChart from "./components/usageAreaChart.tsx";
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path={"login"} element={<LoginPage />}></Route>
                 <Route path={"sign-up"} element={<SignupPage />}></Route>
                 <Route path={"devices"} element={<DevicePage />}></Route>
+                <Route path={"dashboard"} element={<UsageAreaChart width={500} height={500} />}></Route>
                 <Route path={"ProfileManagement"} element={<ProfileManagementPage />}> </Route>
             </Routes>
 
