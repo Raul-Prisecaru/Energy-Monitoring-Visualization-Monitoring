164e46445a5e357f2c3ce1edf4a06685aba1f1bb
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-28 20:34:15 +0000

    Added: Copied over from UsageAreaChart

---
+++ client/src/components/topDevicesBarChart.tsx
@@ -0,0 +1,34 @@
+import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis} from "recharts";
+import {useEffect, useState} from "react";
+
+function TopDevicesBarChart({width, height}) {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/")
+            .then((response) => response.json())
+            .then((devices) => {
+                const deviceHistoryData = devices.flatMap(device =>
+                    device.energyHistory.map(historyData => ({
+                        energyUsage: historyData.energyUsage,
+                        energyDate: new Date(historyData.energyDate).toLocaleDateString("en-GB")
+                    }))
+                );
+                setData(deviceHistoryData);
+            })
+            .catch((error) => console.error("Failed to fetch Devices: " + error));
+    }, []);
+
+    return (
+        <div>
+            <AreaChart width={width} height={height} data={data}>
+                <XAxis dataKey={"energyDate"} />
+                <YAxis />
+                <Area type="monotone" dataKey="energyUsage" />
+            </AreaChart>
+        </div>
+    )
+}
+
+
+export default TopDevicesBarChart;
