40d68ca669ac9004b01f64298c4f32fd3e9beeba
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-28 21:09:33 +0000

    Added: Route for getCategoryEnergy

--- server/routes/deviceRoute.ts
+++ server/routes/deviceRoute.ts
@@ -7,6 +7,9 @@ const router = express.Router();
 // Router to get all devices
 router.get("/", deviceController.getAllDevices)
 
+// router to get all energy Usage of every Category
+router.get("/getCategoryEnergyUsage", deviceController.getCategoryEnergy)
+
 // Router to get one device
 router.get("/:id", deviceController.getOneDevice)
 
