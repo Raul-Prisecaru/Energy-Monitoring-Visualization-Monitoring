4228a70b61ddd977f8a19761a00b9662e9915e4f
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-28 20:33:54 +0000

    Changed: change route path from UsageAreaChart to TopDeviceBarChart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,10 @@ import NavBar from "./components/navBar.tsx";
 import LoginPage from "./pages/loginPage.tsx";
 import SignupPage from "./pages/signupPage.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import DevicePage from "./pages/devicePage.tsx";
+import DeviceManagementPage from "./pages/deviceManagementPage.tsx";
 import ProfileManagementPage from "./pages/profileManagementPage.tsx";
 import UsageAreaChart from "./components/usageAreaChart.tsx";
+import TopDevicesBarChart from "./components/topDevicesBarChart.tsx";
 function App() {
 
   return (
@@ -16,8 +17,8 @@ function App() {
                 <Route path={"/"} element={<HomePage />}></Route>
                 <Route path={"login"} element={<LoginPage />}></Route>
                 <Route path={"sign-up"} element={<SignupPage />}></Route>
-                <Route path={"devices"} element={<DevicePage />}></Route>
-                <Route path={"dashboard"} element={<UsageAreaChart width={500} height={500} />}></Route>
+                <Route path={"devices"} element={<DeviceManagementPage />}></Route>
+                <Route path={"dashboard"} element={<TopDevicesBarChart width={500} height={500} />}></Route>
                 <Route path={"ProfileManagement"} element={<ProfileManagementPage />}> </Route>
             </Routes>
 
