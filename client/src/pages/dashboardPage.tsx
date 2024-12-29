161c42c07c2a53fd3d49a3eaa475270ca17e629b
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-29 03:11:23 +0000

    Added: UsageAreaChart and TopDevicesBarChart

---
+++ client/src/pages/dashboardPage.tsx
@@ -0,0 +1,18 @@
+import UsageAreaChart from "../components/usageAreaChart.tsx";
+import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
+
+function DashboardPage() {
+
+
+    return (
+
+        <div>
+            <UsageAreaChart width={undefined} height={undefined} />
+            <TopDevicesBarChart width={undefined} height={undefined} />
+        </div>
+    )
+
+}
+
+
+export default DashboardPage
