ce3cbe37a518959d5b5a585dc33731a343c45bdc
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-30 16:13:58 +0000

    Added: EnergyCostBarChart to dashboard

--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -2,6 +2,7 @@ import "./styles/dashboardPageStyle.css"
 
 import UsageAreaChart from "../components/usageAreaChart.tsx";
 import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
+import EnergyCostBarChart from "../components/energyCostBarChart.tsx";
 
 function DashboardPage() {
 
@@ -11,6 +12,7 @@ function DashboardPage() {
         <div className={"deviceVisualizer"}>
             <UsageAreaChart width={500} height={300} />
             <TopDevicesBarChart width={500} height={300} />
+            <EnergyCostBarChart />
         </div>
     )
 
