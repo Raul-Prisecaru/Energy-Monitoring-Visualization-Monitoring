b003ef72fdcd9cabfe7af6d025b1710c02bbbce9
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-30 18:34:38 +0000

    Implemented: fetch to retrieve and store energyUsageProgress

--- client/src/components/energyUsageProgressHalfDonutChart.tsx
+++ client/src/components/energyUsageProgressHalfDonutChart.tsx
@@ -1,5 +1,23 @@
-function EnergyUsageProgressHalfDonutChart(width, height) {
+import {useEffect, useState} from "react";
 
+function EnergyUsageProgressHalfDonutChart(width, height) {
+    const [data, setData] = useState()
+    
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getEnergyUsageProgress")
+            .then((response) => response.json())
+            .then((data) => {
+                const formattedData = [
+                     {
+                        totalEnergyUsage: data.total,
+                        limit: data.limit
+                     }
+                ];
+
+                setData(formattedData);
+            })
+            .catch((error) => console.error("Failed to fetch Devices: " + error));
+    }, []);
 
 
     return (
