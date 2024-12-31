d72eb1ec7f2379ebc3b29029ab4549a62854da91
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-31 00:59:47 +0000

    BugFix: Added Default initial state to fix pieChart properly render the PieChart

--- client/src/components/energyUsageProgressHalfDonutChart.tsx
+++ client/src/components/energyUsageProgressHalfDonutChart.tsx
@@ -2,8 +2,15 @@ import {useEffect, useState} from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
 
-function EnergyUsageProgressHalfDonutChart(width, height) {
-    const [data, setData] = useState()
+function EnergyUsageProgressHalfDonutChart() {
+    const [data, setData] = useState({
+        labels: [],
+        datasets: [
+            {
+                data: [],
+            },
+        ],
+    });
 
     ChartJS.register(Tooltip, Legend, ArcElement);
 
@@ -11,16 +18,19 @@ function EnergyUsageProgressHalfDonutChart(width, height) {
         fetch("http://localhost:3001/api/device/getEnergyUsageProgress")
             .then((response) => response.json())
             .then((data) => {
-                const formattedData = [
-                     {
-                         label: ["Used Energy", "Max Target"],
+                const formattedData = {
+                    labels: ["Used Energy", "Max Target"],
 
-                         datasets: [ {
-                             data: [data.total, data.limit]
-                             } ]
+                    datasets: [
+                        {
+                            data: [data.total, data.limit - data.total],
+                            backgroundColor: ["#FF6384", "#36A2EB"],
+                            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+                        }
+                    ]
+                };
 
-                     }
-                ];
+
 
                 setData(formattedData);
             })
