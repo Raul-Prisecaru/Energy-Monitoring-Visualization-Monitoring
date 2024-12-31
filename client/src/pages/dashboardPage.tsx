1b041358265b6a6f30eb3fda3b040d74ae86626e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-31 00:08:00 +0000

    Added: energyUsageProgressHalfDonutChart to the dashboard

--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -3,6 +3,7 @@ import "./styles/dashboardPageStyle.css"
 import UsageAreaChart from "../components/usageAreaChart.tsx";
 import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
 import EnergyCostBarChart from "../components/energyCostBarChart.tsx";
+import EnergyUsageProgressHalfDonutChart from "../components/energyUsageProgressHalfDonutChart.tsx";
 
 function DashboardPage() {
 
@@ -13,6 +14,7 @@ function DashboardPage() {
             <UsageAreaChart width={500} height={300} />
             <TopDevicesBarChart width={500} height={300} />
             <EnergyCostBarChart width={500} height={300}/>
+            <EnergyUsageProgressHalfDonutChart />
         </div>
     )
 
