8c33a6aa6c53d3b7cacd78b53dd92fd55d63ee78
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-07 23:07:53 +0000

    Feat: added EnergyUsageCategoryPieChart

--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -4,6 +4,7 @@ import UsageAreaChart from "../components/usageAreaChart.tsx";
 import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
 import EnergyCostBarChart from "../components/energyCostBarChart.tsx";
 import EnergyUsageProgressHalfDonutChart from "../components/energyUsageProgressHalfDonutChart.tsx";
+import EnergyUsageCategoryPieChart from "../components/energyUsageCategoryPieChart.tsx";
 
 function DashboardPage() {
 
@@ -15,6 +16,7 @@ function DashboardPage() {
             <TopDevicesBarChart width={500} height={300} />
             <EnergyCostBarChart width={500} height={300}/>
             <EnergyUsageProgressHalfDonutChart />
+            <EnergyUsageCategoryPieChart width={500} height={300}/>
         </div>
     )
 
