2c93176c4e6934908712033d1daa865db8d14a27
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-08 17:27:02 +0000

    Feat: Added TotalCostMonthChart to the dashboard

--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -5,6 +5,7 @@ import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
 import EnergyCostBarChart from "../components/energyCostBarChart.tsx";
 import EnergyUsageProgressHalfDonutChart from "../components/energyUsageProgressHalfDonutChart.tsx";
 import EnergyUsageCategoryPieChart from "../components/energyUsageCategoryPieChart.tsx";
+import TotalCostMonthChart from "../components/totalCostMonthChart.tsx";
 
 function DashboardPage() {
 
@@ -13,15 +14,21 @@ function DashboardPage() {
         <div className={"deviceVisualizer"}>
 
             <div className={"costRelatedVisuals"}>
+                <div className={"totalCostMonthChart"}>
+                    <TotalCostMonthChart/>
+                </div>
+
                 <div className={"energyCostBarChart"}>
                     <EnergyCostBarChart width={500} height={300}/>
                 </div>
+
+
             </div>
 
 
             <div className={"deviceRelatedVisuals"}>
                 <div className={"energyUsageAreaChart"}>
-                    <UsageAreaChart width={500} height={300}/>
+                <UsageAreaChart width={500} height={300}/>
                 </div>
 
                 <div className={"topDevicesEnergyUsageBarChart"}>
