1424598f5cbf2138b006b5ab7fa6d316671af619
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-08 23:43:11 +0000

    Feat: added interface

--- client/src/components/energyCostBarChart.tsx
+++ client/src/components/energyCostBarChart.tsx
@@ -1,13 +1,19 @@
 import {useEffect, useState} from "react";
 import {Bar, BarChart, XAxis, YAxis} from "recharts";
 
-function EnergyCostBarChart({width, height}) {
-    const [data, setData] = useState()
+interface formattedData {
+    energyCost: number,
+    energyDate: string
+}
+
+function EnergyCostBarChart({width, height}: {width: number, height: number}) {
+    const [data, setData] = useState<formattedData[]>([])
+
     useEffect(() => {
         fetch("http://localhost:3001/api/device/getEnergyUsageCostPerMonth")
             .then((response) => response.json())
             .then((energyDevice) => {
-                const formattedData = Object.keys(energyDevice).map((energy) => ({
+                const formattedData: formattedData[] = Object.keys(energyDevice).map((energy) => ({
                     energyCost: energyDevice[energy],
                     energyDate: new Date(energy).toLocaleDateString("en-GB"),
                     })
