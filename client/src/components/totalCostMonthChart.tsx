5788443c47dd6a07f064abd157a4323787d4a679
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-08 17:23:34 +0000

    Feat: Added fetch to retrieve cost and displayed the cost

--- client/src/components/totalCostMonthChart.tsx
+++ client/src/components/totalCostMonthChart.tsx
@@ -1,7 +1,20 @@
-function TotalCostMonthChart() {
+import {useEffect, useState} from "react";
 
+function TotalCostMonthChart() {
+    const [data, setData] = useState()
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getCurrentMonthCost")
+            .then((response) => response.json())
+            .then((energyDevice) => {
+                setData(energyDevice.totalEnergy);
+            })
+            .catch((error) => console.error("Failed to fetch Devices: " + error));
+    }, []);
     return (
         <div>
+            <h3>Your Currently Paying: </h3>
+
+            <h1>£{data}</h1>
 
         </div>
     )
