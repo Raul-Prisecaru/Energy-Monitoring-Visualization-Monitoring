b72862d5bf064fa6b3f0b38529ad5f7b5e019480
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-08 23:46:21 +0000

    Feat: added interface

--- client/src/components/energyUsageCategoryPieChart.tsx
+++ client/src/components/energyUsageCategoryPieChart.tsx
@@ -1,14 +1,19 @@
 import {Pie, PieChart} from "recharts";
 import {useEffect, useState} from "react";
 
-function TopDevicesBarChart({width, height}) {
-    const [data, setData] = useState([])
+interface formattedData {
+    deviceCategory: string,
+    energyUsage:number
+}
+
+function TopDevicesBarChart({width, height}: {width: number, height: number}) {
+    const [data, setData] = useState<formattedData[]>([])
 
     useEffect(() => {
         fetch("http://localhost:3001/api/device/getEnergyOfEachCategory")
             .then((response) => response.json())
             .then((devices) => {
-                const formattedData = Object.keys(devices).map((category) => ({
+                const formattedData: formattedData[] = Object.keys(devices).map((category) => ({
                     deviceCategory: category,
                     energyUsage: devices[category]
                 })
