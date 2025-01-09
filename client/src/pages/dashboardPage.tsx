ab831286bd267d857aeea8f4f843d73b32f6e39e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-09 18:26:50 +0000

    Feat: renamed component from EnergyUsageProgressHalfDonutChart to EnergyUsageProgressGauge

--- client/src/pages/dashboardPage.tsx
+++ client/src/pages/dashboardPage.tsx
@@ -3,7 +3,7 @@ import "./styles/dashboardPageStyle.css"
 import UsageAreaChart from "../components/usageAreaChart.tsx";
 import TopDevicesBarChart from "../components/topDevicesBarChart.tsx";
 import EnergyCostBarChart from "../components/energyCostBarChart.tsx";
-import EnergyUsageProgressHalfDonutChart from "../components/energyUsageProgressHalfDonutChart.tsx";
+import EnergyUsageProgressGauge from "../components/energyUsageProgressGauge.tsx";
 import EnergyUsageCategoryPieChart from "../components/energyUsageCategoryPieChart.tsx";
 import TotalCostMonthChart from "../components/totalCostMonthChart.tsx";
 import CostPerDeviceChart from "../components/costPerDeviceChart.tsx"
@@ -40,7 +40,7 @@ function DashboardPage() {
 
 
                 <div className={"energyUsageProgressionHalfDonutChart"}>
-                    <EnergyUsageProgressHalfDonutChart width={550} height={550}/>
+                    <EnergyUsageProgressGauge width={550} height={550}/>
                 </div>
 
                 <div className={"energyUsageCategoryPieChart"}>
