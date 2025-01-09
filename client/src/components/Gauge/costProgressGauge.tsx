a2e3bb0510175f5087ed6f3d2c2158528b880130
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-09 23:45:43 +0000

    Feat: Imported CostProgressGauge Component

---
+++ client/src/components/Gauge/costProgressGauge.tsx
@@ -0,0 +1,57 @@
+import {useEffect, useState} from "react";
+import { Gauge } from '@mui/x-charts/Gauge';
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+
+import "../styles/energyUsageProgressGaugeStyle.css"
+
+
+
+interface formattedData {
+    dataTotal: number,
+    dataCurrent: number
+}
+
+function EnergyUsageProgressGauge({width, height}: {width: number, height: number}) {
+    const [data, setData] = useState<formattedData>({});
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getCurrentMonthCost")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data)
+            })
+            .catch((error) => console.error("Failed to fetch Devices: " + error));
+    }, []);
+
+
+    return (
+        <div className={"gaugeChart"}>
+            <Box sx={{width: 300}}>
+                <Card>
+                    <CardContent>
+                        <Gauge
+                            value={data}
+                            valueMax={100}
+                            startAngle={-110}
+                            endAngle={110}
+                            width={width}
+                            height={height}
+                            text={
+                                ({ value, valueMax }) => `${value} / ${valueMax}`
+                            }
+                        />
+
+                    </CardContent>
+
+                </Card>
+
+            </Box>
+
+        </div>
+    )
+}
+
+
+export default EnergyUsageProgressGauge
