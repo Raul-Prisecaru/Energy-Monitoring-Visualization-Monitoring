da1452b367b77bb63ceed480c9ce635d786924a2
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-10 17:40:39 +0000

    Feat; Removed Box Component

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -1,15 +1,67 @@
 import "./styles/homePageStyle.css"
 import "./styles/background.css"
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import {Typography} from "@mui/joy";
+
+
 function HomePage() {
 
 
     return (
         <div>
             <div className={"Title"}>
-                <h1>Welcome to Your one-stop shop<br/> to managing your energy consumption usage</h1>
+                <h1>Welcome to Your One-Stop Shop<br/> In Managing Your Energy Consumption Usage</h1>
             </div>
             <div className={"getStartedButton"}>
                 <button>Get Started</button>
+
+            </div>
+
+
+            <div className={"Feature"}>
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 1</Typography>
+                            <Typography>Description of the Feature 1</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 2</Typography>
+                            <Typography>Description of the Feature 2</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 3</Typography>
+                            <Typography>Description of the Feature 3</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 4</Typography>
+                            <Typography>Description of the Feature 4</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 5</Typography>
+                            <Typography>Description of the Feature 5</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography level="title-md">Feature 6</Typography>
+                            <Typography>Description of the Feature 6</Typography>
+                        </CardContent>
+                    </Card>
 
             </div>
 
