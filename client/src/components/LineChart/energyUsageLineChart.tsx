70406789c33dbdf96a3313f886e1fe25e223c95c
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-10 02:59:28 +0000

    Feat: Converted from LineChart to barChart

--- client/src/components/LineChart/energyUsageLineChart.tsx
+++ client/src/components/LineChart/energyUsageLineChart.tsx
@@ -1,38 +1,41 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
 import { useEffect, useState } from "react";
 
-interface FormattedData {
-    device: string,
-    energyUsage: number[]
+interface formattedData {
+    device: string;
+    month: number;
+    energyUsage: number;
 }
 
-function EnergyUsageLineChart({ width, height }: { width: number, height: number }) {
+function EnergyUsageLineChart({ width, height }: { width: number; height: number }) {
     const [data, setData] = useState<FormattedData[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:3001/api/device/getEnergyUsageHistoryMonthly")
+        fetch("http://localhost:3001/api/device/getEnergyAveragePerDevice")
             .then((response) => response.json())
             .then((devices) => {
-                const formattedData = Object.keys(devices).map((deviceKey) => {
-                    return {
-                        device: deviceKey,
-                        energyUsage: devices[deviceKey],
-                    };
-                });
+                const formattedData: formattedData[] = Object.keys(devices).map((data) => ({
+                        device: data,
+                        averageEnergy: devices[data]
+                    })
+                );
                 setData(formattedData);
             })
-            .catch((error) => console.error("Failed to fetch Devices: " + error));
+            .catch((error) => console.error("Failed to fetch devices: " + error));
     }, []);
 
     return (
         <div>
-            <LineChart width={width} height={height} data={data}>
-                <XAxis dataKey={"energyDate"} />
-                <YAxis />
-                <Line type="monotone" dataKey="energyUsage" />
-            </LineChart>
+            <BarChart width={width} height={height} data={data}>
+                {/*<CartesianGrid strokeDasharray="3 3" />*/}
+                <XAxis dataKey="device" />
+                <YAxis dataKey={"averageEnergy"}/>
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="averageEnergy" fill="#8884d8" />
+            </BarChart>
         </div>
-    )
+    );
 }
 
 export default EnergyUsageLineChart;
