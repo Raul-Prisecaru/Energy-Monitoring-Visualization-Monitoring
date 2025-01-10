b4a243cd9840983de5b3c6b09b90ba6aabd5c8b2
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-10 03:35:01 +0000

    Feat: Imported Table from @mui/joy/Table and Added Table, thead, tr, th

---
+++ client/src/components/table/deviceTable.tsx
@@ -0,0 +1,44 @@
+import {Pie, PieChart} from "recharts";
+import {useEffect, useState} from "react";
+import Table from '@mui/joy/Table';
+
+interface formattedData {
+    deviceCategory: string,
+    energyUsage:number
+}
+
+function DeviceTable({width, height}: {width: number, height: number}) {
+    const [data, setData] = useState<formattedData[]>([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getEnergyOfEachCategory")
+            .then((response) => response.json())
+            .then((devices) => {
+                const formattedData: formattedData[] = Object.keys(devices).map((category) => ({
+                    deviceCategory: category,
+                    energyUsage: devices[category]
+                })
+                );
+                setData(formattedData);
+            })
+            .catch((error) => console.error("Failed to fetch Devices: " + error));
+    }, []);
+
+    return (
+        <div>
+            <Table>
+                <thead>
+                <tr>
+                    <th> Active </th>
+                    <th> Device </th>
+                    <th> Active Usage </th>
+                </tr>
+                </thead>
+
+            </Table>
+        </div>
+    )
+}
+
+
+export default DeviceTable;
