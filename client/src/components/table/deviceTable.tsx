76b043670659f685d541e06796d0690313e8861f
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-10 04:20:59 +0000

    Feat: Changed fetch to getDeviceActiveStatusAndUsage, updated interface, and updated fetched data

--- client/src/components/table/deviceTable.tsx
+++ client/src/components/table/deviceTable.tsx
@@ -3,22 +3,25 @@ import {useEffect, useState} from "react";
 import Table from '@mui/joy/Table';
 
 interface formattedData {
-    deviceCategory: string,
-    energyUsage:number
+    deviceName: string,
+    deviceStatus: boolean,
+    deviceEnergy: number
 }
 
 function DeviceTable({width, height}: {width: number, height: number}) {
     const [data, setData] = useState<formattedData[]>([])
 
     useEffect(() => {
-        fetch("http://localhost:3001/api/device/getEnergyOfEachCategory")
+        fetch("http://localhost:3001/api/device/getDeviceActiveStatusAndUsage")
             .then((response) => response.json())
             .then((devices) => {
-                const formattedData: formattedData[] = Object.keys(devices).map((category) => ({
-                    deviceCategory: category,
-                    energyUsage: devices[category]
+                const formattedData: formattedData[] = Object.keys(devices).map((data) => ({
+                    deviceName: data,
+                    deviceStatus: devices[data][0],
+                    deviceEnergy: devices[data][1],
                 })
                 );
+                console.log(formattedData)
                 setData(formattedData);
             })
             .catch((error) => console.error("Failed to fetch Devices: " + error));
