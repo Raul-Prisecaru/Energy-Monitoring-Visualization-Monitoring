663d39bc38dc74828e6a8acc30aeba83e440a4fd
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 00:38:02 +0000

    Feat: Created costCard function and imported Box, Card, CardContent, Typography from @mui/joy

---
+++ client/src/pages/deviceVisualComponents/costCard.tsx
@@ -0,0 +1,17 @@
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+
+
+function CostCard() {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CostCard
