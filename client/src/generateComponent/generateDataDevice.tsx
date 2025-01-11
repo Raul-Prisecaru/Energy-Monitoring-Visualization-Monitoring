ce0660d15d017b683b9c1051273eae8db4fdbb86
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 03:23:07 +0000

    Feat: Tested with a simple counter between dates

--- client/src/generateComponent/generateDataDevice.tsx
+++ client/src/generateComponent/generateDataDevice.tsx
@@ -1,7 +1,15 @@
-function GenerateDataDevice({deviceID, startYear, endYear}: {deviceID: string, startYear: number, endYear: number}) {
+function GenerateDataDevice({ deviceID, startYear }: { deviceID: string; startYear: number;}) {
+    const startDate: Date = new Date(`${startYear}-01-01`);
+    const endDate: Date = new Date(`${startYear}-12-31`);
+    let currentDate: Date = new Date(startDate);
 
+    let counter: number = 0;
+
+    while (currentDate <= endDate) {
+        console.log(counter);
+        currentDate.setDate(currentDate.getDate() + 1);
+        counter += 1;
+    }
 
 }
-
-
 export default GenerateDataDevice
