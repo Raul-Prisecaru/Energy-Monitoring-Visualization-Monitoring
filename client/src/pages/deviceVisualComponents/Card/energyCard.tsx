5fe6465b50680a96f8feb38945e5bde813535de4
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 01:11:16 +0000

    Feat: Duplicated costCard and replaced cost with energy Usage

---
+++ client/src/pages/deviceVisualComponents/Card/energyCard.tsx
@@ -0,0 +1,48 @@
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import {useEffect, useState} from "react";
+
+
+interface formattedData {
+    deviceName: string,
+    energy: number
+}
+
+function EnergyCard({deviceID}: {deviceID: string}) {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getDayEnergyUsageDevice/"+ deviceID)
+            .then(response => response.json())
+            .then(dataSet => {
+                const formattedData: formattedData[] = Object.keys(dataSet).map((device) => ({
+                        deviceName: device,
+                        energy: dataSet[device]
+                    })
+                );
+                console.log(formattedData)
+                setData(formattedData)
+            })
+    }, []);
+
+    return (
+        <div className={"cardComponent"}>
+            <Box>
+                <Card>
+                    <CardContent>
+                        {data.map((device) => (
+                            <div key={device.deviceName}>
+                                <Typography level="title-md"> Today's energy usage of {device.deviceName}</Typography>
+                                <Typography>energy Usage: {device.energy} kWh</Typography>
+                            </div>
+                        ))}
+                    </CardContent>
+                </Card>
+            </Box>
+        </div>
+    )
+}
+
+export default EnergyCard
