5a20a5d76677e748fdb1fe70a39ff648c8db40ab
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 00:45:32 +0000

    Feat: added fetch to retrieve and process the data to be stored in useState and added formattedData interface

--- client/src/pages/deviceVisualComponents/costCard.tsx
+++ client/src/pages/deviceVisualComponents/costCard.tsx
@@ -2,10 +2,29 @@ import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
+import {useEffect, useState} from "react";
+import {response} from "express";
 
+interface formattedData {
+    deviceName: string,
+    cost: number
+}
 
 function CostCard() {
+    const [data, setData] = useState([])
 
+    useEffect(() => {
+        fetch("https://localhost:3001/api/device/getDayCostDevice/")
+            .then(response => response.json())
+            .then(dataSet => {
+                const formattedData: formattedData[] = Object.keys(dataSet).map((device) => ({
+                        deviceName: device,
+                        cost: dataSet[device]
+                    })
+                );
+                setData(formattedData)
+            })
+    }, []);
 
     return (
         <div>
