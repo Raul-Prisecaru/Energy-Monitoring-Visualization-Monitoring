71d1483c03d5b16fa3e56215dd45f8b6f05787c7
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 01:55:37 +0000

    Feat: fetched and formatted data and stored in data useState and added interface for formattedData

--- client/src/pages/deviceVisualComponents/BarChart/EnergyHistoryBarChart.tsx
+++ client/src/pages/deviceVisualComponents/BarChart/EnergyHistoryBarChart.tsx
@@ -1,6 +1,24 @@
-function EnergyHistoryBarChart() {
+import {useEffect, useState} from "react";
 
+interface formattedData {
+    month: string,
+    energyUsage: number
+}
 
+function EnergyHistoryBarChart({deviceID}: {deviceID: string}) {
+    const [data, setData] = useState<formattedData[]>([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getEnergyHistoryDevice/" + deviceID)
+            .then(response => response.json())
+            .then(dataSet => {
+                const formattedData: formattedData[] = Object.keys(dataSet).map((dataMonth) => ({
+                    month: dataMonth,
+                    energyUsage: dataSet[dataMonth]
+                }))
+                setData(formattedData)
+            })
+    }, []);
     return (
         <div>
 
