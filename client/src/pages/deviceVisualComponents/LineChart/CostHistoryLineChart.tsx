8faea259ff73493659467776c0919cb4b5797f14
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 02:34:58 +0000

    Feat: Fetched and processed data and stored in useState

--- client/src/pages/deviceVisualComponents/LineChart/CostHistoryLineChart.tsx
+++ client/src/pages/deviceVisualComponents/LineChart/CostHistoryLineChart.tsx
@@ -2,9 +2,22 @@ import {LineChart} from "recharts";
 import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
+import {useEffect, useState} from "react";
 
-function CostHistoryLineChart() {
+function CostHistoryLineChart({deviceID}: {deviceID: string}) {
+    const [data, setData] = useState([])
 
+    useEffect(() => {
+        fetch("http://localhost:3001/api/device/getCostHistoryDevice" + deviceID)
+            .then(response => response.json())
+            .then(dataSet => {
+                const formattedData = Object.keys(dataSet).map(month => ({
+                    month: month,
+                    cost: dataSet[month]
+                }))
+                setData(data)
+            })
+    }, []);
 
     return (
         <div>
