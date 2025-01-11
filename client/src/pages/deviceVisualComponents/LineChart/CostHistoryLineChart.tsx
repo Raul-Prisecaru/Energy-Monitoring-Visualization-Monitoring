741813f5ff22b02397bcac4b31c6ca60946f0816
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 02:30:55 +0000

    Feat: Created CostHistoryLineChart component and imported Box, Card, CardContent and LineChart

---
+++ client/src/pages/deviceVisualComponents/LineChart/CostHistoryLineChart.tsx
@@ -0,0 +1,23 @@
+import {LineChart} from "recharts";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+
+function CostHistoryLineChart() {
+
+
+    return (
+        <div>
+            <Box>
+                <Card>
+                    <CardContent>
+
+                    </CardContent>
+                </Card>
+            </Box>
+        </div>
+    )
+
+}
+
+export default CostHistoryLineChart
