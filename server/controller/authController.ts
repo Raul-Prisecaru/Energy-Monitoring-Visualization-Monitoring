01279d17da0e882c4fa8f1bed16bfaa98aa386eb
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-11 23:41:21 +0000

    Feat: retrieved username and password from request body, and applied findOne to find the username

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -2,8 +2,11 @@ import jwt from "jsonwebtoken"
 import User from "../model/userAccount"
 
 export const loginUser = async (req, res) => {
-    try {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    try {
+        const foundUser = await User.findOne({ username })
 
     } catch (err) {
         res.status(500).json({err: "An Error occurred during login"})
