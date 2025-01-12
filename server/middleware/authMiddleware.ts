d0c67348dd8c8ffe858f38b064a5a0497c3c5a2e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-12 07:20:43 +0000

    Feat: Collected token from authentication Header and applied jwt to verify token

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,6 +1,19 @@
-const jwt = require('jsonwebtoken');
+import * as jwt from 'jsonwebtoken';
 
-export const authMiddleware = (req, res, next) => {
+export const authMiddleware = (req: any, res: any, next: any) => {
+    const authHeader = req.header('Authorization');
 
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        const token = authHeader.split(' ')[1];
+        try {
+            const verified: any = jwt.verify(token, "test");
+            req.user = verified;
+            next();
+        } catch (error) {
+            console.error("Error verifying token: ", error);
+            res.status(400).json({ message: 'Invalid Token' });
+        }
+    } else {
+        res.status(401).json({ message: 'Access Denied' });
+    }
 };
-
