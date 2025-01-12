2f98d24ebb0ea122e820cfad9a9128f6e3978cb4
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-12 17:28:51 +0000

    Feat: added middleware to /profile route

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { loginUser, signUser , isAuthenticated } from '../controller/authController';
-
+import { loginUser, signUser, profileUser } from '../controller/authController';
+import {authMiddleware} from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.post('/loginUser', loginUser);
 router.post('/signUser', signUser);
+
+router.get("/profile", authMiddleware, profileUser)
 
 
 export default router
