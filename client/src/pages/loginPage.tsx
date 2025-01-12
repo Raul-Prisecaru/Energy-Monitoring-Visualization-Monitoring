dea6aff54d0be617496c4ae8791d14bdccf0fa01
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-12 03:44:17 +0000

    Feat: Added handleUsernameChange and handlePasswordChange, added buttonPress to make a login request

--- client/src/pages/loginPage.tsx
+++ client/src/pages/loginPage.tsx
@@ -1,20 +1,59 @@
 import "./styles/loginPageStyle.css"
+import {useState} from "react";
 
 function LoginPage() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+    const buttonPress = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3001/api/auth/loginUser", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+
+            if (response.ok) {
+                alert("Logged in")
+            } else {
+                alert("Unable to login")
+            }
+        } catch (error) {
+            alert("An Error occurred when trying to login:" + error)
+        }
+    }
 
     return (
         <div className={"Input-Form"}>
             <form>
                 <div>
-                    <label>Email</label>
-                    <input type={"text"}/> <br/>
+                    <label>Username</label>
+                    <input onChange={handleUsernameChange} type={"text"}/> <br/>
                 </div>
 
                 <div>
                     <label>password</label>
-                    <input type={"text"}/>
+                    <input onChange={handlePasswordChange} type={"text"}/>
                 </div>
 
+
+                <button onClick={buttonPress}>Login</button>
             </form>
         </div>
     )
