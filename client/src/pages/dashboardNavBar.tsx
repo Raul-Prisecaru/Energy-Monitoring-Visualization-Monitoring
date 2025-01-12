a7d8b8ca4a5c7b21e909961c3f2050bcba1ac587
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-12 16:26:47 +0000

    Feat: created dashboardNavBar to display dashboard and menu

---
+++ client/src/pages/dashboardNavBar.tsx
@@ -0,0 +1,35 @@
+import {Button, Dropdown, Menu, MenuButton, MenuItem, Typography} from "@mui/joy";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+function DashboardNavBar() {
+    const navigate = useNavigate();
+
+    return (
+
+        <div className={"LoginNavBar"}>
+            <div className={"TopNavBar"}>
+                <div className={"TopNavOptions"}>
+                    <Button onClick={() => navigate("/Dashboard")} variant={"plain"}>
+                        <Typography level="h4">
+                            Dashboard
+                        </Typography>
+                    </Button>
+
+                    <Dropdown >
+                        <MenuButton>Menu</MenuButton>
+                        <Menu>
+                            <MenuItem onClick={() => navigate("/ProfileManagement")}>Profile Management</MenuItem>
+                            <MenuItem onClick={() => localStorage.removeItem("token")}>Logout</MenuItem>
+                        </Menu>
+                    </Dropdown>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+
+export  default DashboardNavBar
