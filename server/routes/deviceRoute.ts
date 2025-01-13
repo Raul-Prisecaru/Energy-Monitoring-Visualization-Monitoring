9ebd2a9b563be4b51df0e13099fe48e705c128bd
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-13 13:58:35 +0000

    Feat: made all routes protected

--- server/routes/deviceRoute.ts
+++ server/routes/deviceRoute.ts
@@ -12,7 +12,7 @@ router.get("/", authMiddleware, deviceController.getAllDevices)
 router.get("/getEnergyOfEachCategory", authMiddleware, deviceController.getEnergyOfEachCategory)
 
 // Router to get cost produced per device
-router.get("/getCostPerDevice", deviceController.getCostPerDevice)
+router.get("/getCostPerDevice", authMiddleware, deviceController.getCostPerDevice)
 
 // Router to get the total energy Usage of the month
 router.get("/getCurrentMonthEnergyUsage", authMiddleware,  deviceController.getCurrentMonthEnergyUsage)
@@ -22,18 +22,20 @@ router.get("/getCurrentMonthEnergyUsage", authMiddleware,  deviceController.getC
 // Router to get current paying cost of the current month
 router.get("/getCurrentMonthCost", authMiddleware, deviceController.getCurrentMonthCost)
 
-router.get("/getSpecifiedMonthCost/:month/:year", deviceController.getSpecifiedMonthCost)
+router.get("/getSpecifiedMonthCost/:month/:year", authMiddleware, deviceController.getSpecifiedMonthCost)
 
-router.get("/getCurrentYearCost", deviceController.getCurrentYearCost)
+// router.get("/getCurrentYearCost", authMiddleware, deviceController.getCurrentYearCost)
 
-router.get("/getSpecifiedYearCost/:year", deviceController.getSpecifiedYearCost)
+router.get("/getSpecifiedYearCost/:year", authMiddleware, deviceController.getSpecifiedYearCost)
 
+
+router.get("/getCostMonthGaugeChart", authMiddleware, deviceController.getCostMonthGaugeChart)
 
 // Router to get energy Cost of every date
-router.get("/getEnergyUsageCostPerMonth", deviceController.getEnergyUsageCostPerMonth)
+router.get("/getEnergyUsageCostPerMonth", authMiddleware, deviceController.getEnergyUsageCostPerMonth)
 
 // Router to get ordered list of most energy consumping devices
-router.get("/getTopEnergyUsageDevices", deviceController.getTopEnergyUsageDevices)
+router.get("/getTopEnergyUsageDevices", authMiddleware, deviceController.getTopEnergyUsageDevices)
 
 // Router to get the energy Usage progress
 router.get("/getMonthEnergyUsageProgress", authMiddleware, deviceController.getMonthEnergyUsageProgress)
@@ -41,47 +43,49 @@ router.get("/getMonthEnergyUsageProgress", authMiddleware, deviceController.getM
 // Router to get the cost history Monthly
 router.get("/getCostHistoryMonthly",authMiddleware, deviceController.getCostHistoryMonthly)
 
+// Router to get the average energy usage and cost of every month
 router.get("/getMonthlyEnergyAndCostAveragePerDevice", authMiddleware ,deviceController.getMonthlyEnergyAndCostAveragePerDevice)
 
+// Router to get status of devices
 router.get("/getDeviceActiveStatusAndUsage", authMiddleware, deviceController.getDeviceActiveStatusAndUsage)
 
 // Router to get one device
 router.get("/:id", authMiddleware ,deviceController.getOneDevice)
 
 // Router to get cost history of the current day
-router.get("/getDayCostDevice/:id", deviceController.getDayCostDevice)
+router.get("/getDayCostDevice/:id", authMiddleware, deviceController.getDayCostDevice)
 
 // Router to get cost history of the current month
-router.get("/getMonthCostDevice/:id", deviceController.getMonthCostDevice)
+// router.get("/getMonthCostDevice/:id",authMiddleware,  deviceController.getMonthCostDevice)
 
 // Router to get cost history of the current year
-router.get("/getYearCostDevice/:id", deviceController.getYearCostDevice)
+// router.get("/getYearCostDevice/:id", authMiddleware, deviceController.getYearCostDevice)
 
 // Router to get energy usage history of current day
-router.get("/getDayEnergyUsageDevice/:id", deviceController.getDayEnergyUsageDevice)
+router.get("/getDayEnergyUsageDevice/:id", authMiddleware, deviceController.getDayEnergyUsageDevice)
 
 // Router to get energy usage history of current month
-router.get("/getMonthEnergyUsageDevice/:id", deviceController.getMonthEnergyUsageDevice)
+// router.get("/getMonthEnergyUsageDevice/:id", authMiddleware, deviceController.getMonthEnergyUsageDevice)
 
 // Router to get energy usage history of current year
-router.get("/getYearEnergyUsageDevice/:id", deviceController.getYearEnergyUsageDevice)
+// router.get("/getYearEnergyUsageDevice/:id", authMiddleware, deviceController.getYearEnergyUsageDevice)
 
 // Router to get energy usage history of the device
-router.get("/getEnergyHistoryDevice/:id", deviceController.getEnergyHistoryDevice)
+router.get("/getEnergyHistoryDevice/:id", authMiddleware, deviceController.getEnergyHistoryDevice)
 
 // Router to get cost history of the device
-router.get("/getCostHistoryDevice/:id", deviceController.getCostHistoryDevice)
+router.get("/getCostHistoryDevice/:id", authMiddleware, deviceController.getCostHistoryDevice)
 
 // Router to create device
 router.post("/", authMiddleware , deviceController.createDevice)
 
-router.patch("/:id", deviceController.addEnergyHistory)
+router.patch("/:id", authMiddleware, deviceController.addEnergyHistory)
 
 // Router to update device, Takes ID Param
-router.put("/:id", deviceController.updateDevice)
+router.put("/:id", authMiddleware, deviceController.updateDevice)
 
 // Router to delete device, Takes ID Param
-router.delete("/:id", deviceController.deleteDevice)
+router.delete("/:id", authMiddleware, deviceController.deleteDevice)
 
 // Export Router
 export default router
