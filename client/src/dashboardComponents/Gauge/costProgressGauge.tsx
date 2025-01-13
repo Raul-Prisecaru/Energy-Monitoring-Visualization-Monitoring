5e8b8f61a4f7d61412eba3c545fdb4448123f3f7
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-13 13:56:47 +0000

    Feat: Formatted Data

--- client/src/dashboardComponents/Gauge/costProgressGauge.tsx
+++ client/src/dashboardComponents/Gauge/costProgressGauge.tsx
@@ -1,43 +1,45 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Gauge } from '@mui/x-charts/Gauge';
 import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
-import {Typography} from "@mui/joy";
-
+import { Typography } from "@mui/joy";
 
 interface formattedData {
-    dataTotal: number,
-    dataCurrent: number
+    cost: number,
+    userGoal: number
 }
 
-function EnergyUsageProgressGauge({width, height}: {width: number, height: number}) {
-    const [data, setData] = useState<formattedData>({});
+function EnergyUsageProgressGauge({ width, height }: { width: number, height: number }) {
+    const [data, setData] = useState<formattedData>({ cost: 0, userGoal: 0 });
 
     useEffect(() => {
-        const token = localStorage.getItem("token")
-        fetch("http://localhost:3001/api/device/getCurrentMonthCost", {
+        const token = localStorage.getItem("token");
+        fetch("http://localhost:3001/api/device/getCostMonthGaugeChart", {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         })
             .then((response) => response.json())
             .then((data) => {
-                setData(data)
+                const formattedData: formattedData = ({
+                    cost: data.cost,
+                    userGoal: data.target
+                });
+                setData(formattedData);
             })
             .catch((error) => console.error("Failed to fetch Devices: " + error));
     }, []);
 
-
     return (
         <div className={"gaugeChart"}>
-            <Box sx={{width: 250}}>
+            <Box sx={{ width: 250 }}>
                 <Card>
                     <Typography level={"h3"}>Currently Paying Over Goal </Typography>
                     <CardContent>
                         <Gauge
-                            value={data}
-                            valueMax={300}
+                            value={data.cost}
+                            valueMax={data.userGoal}
                             startAngle={-110}
                             endAngle={110}
                             width={width}
@@ -46,16 +48,11 @@ function EnergyUsageProgressGauge({width, height}: {width: number, height: numbe
                                 ({ value, valueMax }) => `${value} / ${valueMax}`
                             }
                         />
-
                     </CardContent>
-
                 </Card>
-
             </Box>
-
         </div>
-    )
+    );
 }
 
-
-export default EnergyUsageProgressGauge
+export default EnergyUsageProgressGauge;
