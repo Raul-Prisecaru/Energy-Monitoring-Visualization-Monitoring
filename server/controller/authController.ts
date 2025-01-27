5af04c0a3a6c610a1d23f3babc90a0af121fca9d
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-27 08:35:20 +0000

    Feat: added change password function

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -9,7 +9,7 @@ export const loginUser = async (req: any, res: any) => {
     const password = req.body.password;
 
     try {
-        const foundUser = await User.findOne({ username });
+        const foundUser: any = await User.findOne({ username });
 
         if (!foundUser) {
             return res.status(401).json({ message: 'Invalid username or password' });
@@ -25,7 +25,7 @@ export const loginUser = async (req: any, res: any) => {
             return res.status(500).json({ message: 'Server configuration error' });
         }
 
-        const token = jwt.sign({ id: foundUser._id.toString() }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ id: foundUser._id.toString() }, process.env.JWT_SECRET, { expiresIn: "10h" });
 
         res.status(200).json({token});
 
@@ -63,6 +63,37 @@ export const isAuthenticated = (req:any, res:any) => {
         res.json({ message: 'Authenticated', user: req.user });
     } else {
         res.status(401).json({ message: 'Not authenticated' });
+    }
+};
+
+export const changePassword = async (req: any, res: any) => {
+    try {
+        const { currentPassword, newPassword } = req.body;
+        const findUser: any = await User.findById(req.user.id);
+
+        if (!findUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (!currentPassword) {
+            return res.status(400).json({ error: "Please Provide Your Password" });
+        }
+
+        if (!newPassword) {
+            return res.status(400).json({ error: "Please Provide a New Password" });
+        }
+
+        const validatedPassword: boolean = await bcrypt.compare(currentPassword, findUser.password);
+
+        if (!validatedPassword) {
+            return res.status(401).json({ error: "Incorrect current password" });
+        }
+
+        await findUser.changePassword(newPassword);
+
+        res.status(200).json({ success: "Successfully Changed Password" });
+    } catch (err: any) {
+        res.status(500).json({ error: "Failed to Reset Password: " + err.message });
     }
 };
 
