3fc2745bbde1b74289422da854d841e2a2cbdbcd
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-27 08:36:08 +0000

    Feat: added changePassword post router

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { loginUser, signUser, profileUser } from '../controller/authController';
+import {loginUser, signUser, profileUser, changePassword} from '../controller/authController';
 import {authMiddleware} from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.post('/loginUser', loginUser);
 router.post('/signUser', signUser);
+router.post("/changePassword", authMiddleware, changePassword)
 
 router.get("/profile", authMiddleware, profileUser)
 
