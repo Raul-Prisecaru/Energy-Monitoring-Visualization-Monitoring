e548a6abdc21c6ae1a21ec4dbd1c8a1e4f26e27f
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-30 01:41:04 +0000

    Feat: Revamped Settings Design

--- client/src/pages/settingsPage.tsx
+++ client/src/pages/settingsPage.tsx
@@ -1,17 +1,32 @@
 import "./styles/settingsPageStyle.css"
 import { useNavigate } from 'react-router-dom';
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import {Typography} from "@mui/joy";
+import Button from "@mui/joy/Button";
 
 function SettingsPage() {
     const navigate = useNavigate();
 
     return (
-        <div className={"buttonsManagement"}>
-            <div>
-                <button onClick={() => navigate("/ProfileManagement")}> Profile Management </button>
+        <div className={"Management"}>
+            <div className={"ProfileManagementCard"}>
+                <Card sx={{width: "500px"}}>
+                    <CardContent>
+                        <Typography level={"h1"}>Profile Management</Typography>
 
+                        <Button onClick={() => navigate("/ProfileManagement")}> Profile Management </Button>
+                    </CardContent>
+                </Card>
             </div>
-            <div>
-                <button> Notification management </button>
+            <div className={"OtherManagementCard"}>
+                <Card sx={{width: "500px"}}>
+                    <CardContent>
+                        <Typography level={"h1"}>Cost/Goals</Typography>
+
+                        <Button onClick={() => navigate("/ProfileManagement")}> Cost/Goals </Button>
+                    </CardContent>
+                </Card>
             </div>
         </div>
     )
