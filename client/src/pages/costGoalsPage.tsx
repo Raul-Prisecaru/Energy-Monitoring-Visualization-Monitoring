a7c9b84fd5b246b0f583930db0e9e3eebdf2611e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-02-11 00:58:44 +0000

    Feat: connected to api to allow user to change their current settings

--- client/src/pages/costGoalsPage.tsx
+++ client/src/pages/costGoalsPage.tsx
@@ -6,6 +6,7 @@ import Button from "@mui/joy/Button";
 import Modal from "@mui/joy/Modal";
 
 
+
 function CostsGoalsPage() {
     const [currentUserEnergyCost, setCurrentUserEnergyCost] = useState(0);
     const [currentUserEnergyGoal, setCurrentUserEnergyGoal] = useState(0);
@@ -15,31 +16,96 @@ function CostsGoalsPage() {
     const [energyGoalModal, setEnergyGoalModal] = useState(false)
     const [userCostGoalModal, setUserCostGoalModal] = useState(false)
 
-    // const [newUserEnergyCost, setNewUserEnergyCost] = useState(0);
-    // const [newUserEnergyGoal, setNewUserEnergyGoal] = useState(0);
-    // const [newUserCostGoal, setNewUserCostGoal] = useState(0);
+    const [newUserCurrentEnergyCost, setNewUserCurrentEnergyCost] = useState(0)
+    const [newEnergyCostGoal, setNewEnergyCostGoal] = useState(0)
+    const [newEnergyUsageGoal, setNewEnergyUsageGoal] = useState(0)
 
     useEffect(() => {
         try {
-        const token = localStorage.getItem("token")
-        fetch("http://localhost:3001/api/user/getUserPriceCostSettings" ,{
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        })
-            .then((response) => response.json())
-            .then((userCost) => {
-                setCurrentUserEnergyCost(userCost.pricePerkWh);
-                setCurrentUserEnergyGoal(userCost.energyGoals);
-                setCurrentUserCostGoal(userCost.costGoals);
+            const token = localStorage.getItem("token")
+            fetch("http://localhost:3001/api/user/getUserPriceCostSettings" ,{
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
             })
-            .catch((error) => console.error("Failed to retrieve user payment: " + error));
+                .then((response) => response.json())
+                .then((userCost) => {
+                    setCurrentUserEnergyCost(userCost.pricePerkWh);
+                    setCurrentUserEnergyGoal(userCost.costGoals);
+                    setCurrentUserCostGoal(userCost.energyGoals);
+                })
+                .catch((error) => console.error("Failed to retrieve user payment: " + error));
         } catch (err) {
             console.log("Error", err)
         }
 
     }, []);
+
+
+    const updateUserPayment = () => {
+        try {
+            const token = localStorage.getItem("token")
+            fetch("http://localhost:3001/api/user/updateUserCostEnergyPaying/" ,{
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+
+                body: JSON.stringify({
+                    "newCost": newUserCurrentEnergyCost,
+                }),
+            })
+                .catch((error) => console.error("Failed to retrieve user payment: " + error));
+        } catch (err) {
+            console.log("Error", err)
+        }
+    }
+
+
+    const updateUserCostGoal = () => {
+        try {
+            const token = localStorage.getItem("token")
+            fetch("http://localhost:3001/api/user/updateUserCostGoal/" ,{
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+
+                body: JSON.stringify({
+                    "userCostGoal": newEnergyCostGoal,
+                }),
+            })
+                .catch((error) => console.error("Failed to retrieve user payment: " + error));
+        } catch (err) {
+            console.log("Error", err)
+        }
+    }
+
+    const updateUserEnergyUsageGoal = () => {
+        try {
+            const token = localStorage.getItem("token")
+            console.log(token)
+            console.log(newEnergyUsageGoal)
+            fetch("http://localhost:3001/api/user/updateUserUsageEnergyGoal/" ,{
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+
+                body: JSON.stringify({
+                    "newCostGoal": newEnergyUsageGoal,
+                }),
+            })
+                .catch((error) => console.error("Failed to retrieve user payment: " + error));
+        } catch (err) {
+            console.log("Error", err)
+        }
+    }
+
 
 
     return (
@@ -48,8 +114,8 @@ function CostsGoalsPage() {
             <div>
                 <Card>
                     <CardContent>
-                        <Typography level={"h1"}>Cost</Typography>
-                        <Typography>Currently Paying Rate towards Energy: {currentUserEnergyCost} / 1000h</Typography>
+                        <Typography level={"h1"}>Paying Cost Goals</Typography>
+                        <Typography>Currently Paying Rate towards Energy: £{currentUserEnergyCost} / 1000h</Typography>
                         <Button onClick={() => setUserCostGoalModal(true)}>Update</Button>
                     </CardContent>
                 </Card>
@@ -58,7 +124,7 @@ function CostsGoalsPage() {
             <div>
                 <Card>
                     <CardContent>
-                        <Typography level={"h1"}>Cost Goals</Typography>
+                        <Typography level={"h1"}>Energy Usage Goals</Typography>
                         <Typography>Current Energy Usage Goal: {currentUserCostGoal} kWh</Typography>
                         <Button onClick={() => setEnergyGoalModal(true)}>Update</Button>
                     </CardContent>
@@ -69,7 +135,7 @@ function CostsGoalsPage() {
             <div>
                 <Card>
                     <CardContent>
-                        <Typography level={"h1"}>Energy Goals</Typography>
+                        <Typography level={"h1"}>Energy Cost Goals</Typography>
                         <Typography>Currently Energy Cost Goal: £{currentUserEnergyGoal}</Typography>
                         <Button onClick={() => setEnergyCostModal(true)}>Update</Button>
                     </CardContent>
@@ -86,8 +152,8 @@ function CostsGoalsPage() {
                     <CardContent>
                         <Typography level={"h1"}>Update User Payment</Typography>
                         <Typography>How much are you paying now?</Typography>
-                        <Input />
-                        <Button>Update</Button>
+                        <Input onChange={(e) => setNewUserCurrentEnergyCost(Number(e.target.value))}/>
+                        <Button onClick={() => updateUserPayment}>Update</Button>
                     </CardContent>
                 </Card>
                 </Modal>
@@ -103,28 +169,12 @@ function CostsGoalsPage() {
                     <CardContent>
                         <Typography level={"h1"}>Update your energy (kWh) goal</Typography>
                         <Typography>What is your new energy (kWh) goal?</Typography>
-                        <Input />
+                        <Input onChange={(e) => setNewEnergyUsageGoal(Number(e.target.value))} />
+                        <Button onClick={() => updateUserEnergyUsageGoal()}>Update</Button>
                     </CardContent>
                 </Card>
                 </Modal>
             </div>
-
-
-            <div>
-                <Modal
-                open={userCostGoalModal}
-                onClose={() => setUserCostGoalModal(false)}
-                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Card>
-                    <CardContent>
-                        <Typography level={"h1"}>Update User Payment</Typography>
-                        <Typography>How much are you paying now?</Typography>
-                        <Input />
-                    </CardContent>
-                </Card>
-                </Modal>
-            </div>
-
 
             <div>
                 <Modal
@@ -135,7 +185,9 @@ function CostsGoalsPage() {
                     <CardContent>
                         <Typography level={"h1"}>Update Energy Cost Goal</Typography>
                         <Typography>What is your energy cost goal?</Typography>
-                        <Input />
+
+                        <Input onChange={(e) => setNewEnergyCostGoal(Number(e.target.value))}/>
+                        <Button onClick={() => updateUserCostGoal()}>Update</Button>
                     </CardContent>
                 </Card>
                 </Modal>
